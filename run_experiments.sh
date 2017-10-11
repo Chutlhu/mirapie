87d763f61af5024645dee81aafd3600d7d4d8f5c
Chutlhu <diego.dicarlo89@gmail.com>
2017-10-11 14:32:29 +0200

    results

--- run_experiments.sh
+++ run_experiments.sh
@@ -3,7 +3,7 @@ echo "<<<<<<<<<<<<<<<<<<<<<<<<<<<<"
 echo "         START HERE"
 echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
 
-python mirapie.py toydata/ toydata/initL.csv -m 3 -p 1 
+python mirapie.py toydata/ toydata/initL.csv -m 3 -p 1 >> results.txt 
 
 echo "<<<<<<<<<<<<<<<<<<<<<<<<<<<<"
 echo "         FINISH!!!!"
